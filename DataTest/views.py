0243d29b628a61c0b08049dd723f3fa1d88e041c
DataScienceSportAgri <xavier-martin@outlook.com>
2024-11-14 11:29:26 +0100

    premier essai migration global

--- DataTest/views.py
+++ DataTest/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from graph.models import HalfMarathon
 def home(request):
     return render(request, 'home.html')
 
-def halfmarathon_list(request):
-    halfmarathons = HalfMarathon.objects.all()
-    return render(request, 'templates/halfmarathon_list.html', {'halfmarathons': halfmarathons})
--- graph/models.py
+++ graph/models.py
@@ -1,11 +1,42 @@
 from django.db import models
+import uuid
 
-class HalfMarathon(models.Model):
-    # Définissez ici les champs correspondant à votre table
-    # Par exemple :
+
+
+class CategorieCoureur(models.Model):
     nom = models.CharField(max_length=100)
-    temps = models.TimeField()
-    # Ajoutez d'autres champs selon votre structure
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.nom
+
+class Coureur(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    categorie = models.ForeignKey(CategorieCoureur, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
+
+class Course(models.Model):
+    nom = models.CharField(max_length=255)
+    date = models.DateField()
+    distance = models.FloatField()
+
+    def __str__(self):
+        return f"{self.nom} ({self.date})"
+
+class ResultatCourse(models.Model):
+    coureur = models.ForeignKey(Coureur, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    temps = models.DurationField()
+    temps2 = models.DurationField()
+    Vitesse = models.FloatField()
+    position = models.IntegerField()
+
+    class Meta:
+        unique_together = ('coureur', 'course')
+
+    def __str__(self):
+        return f"{self.coureur} - {self.course}"
+
